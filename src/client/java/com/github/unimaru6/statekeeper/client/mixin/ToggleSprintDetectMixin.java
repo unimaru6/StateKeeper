b5805336e91e99f61586779e4d08d4d3af393662
uniun <uniuniunimaru6@gmail.com>
2025-11-10 19:28:28 +0900

    Injectionの位置をTAILに変更

--- src/client/java/com/github/unimaru6/statekeeper/client/mixin/ToggleSprintDetectMixin.java
+++ src/client/java/com/github/unimaru6/statekeeper/client/mixin/ToggleSprintDetectMixin.java
@@ -13,15 +13,13 @@ import com.github.unimaru6.statekeeper.client.StatekeeperClient;
 @Mixin(KeyBinding.class)
 public class ToggleSprintDetectMixin {
 
-    // public static boolean isSprintState = false;
-
     /**
      * called when the sprint key is pressed or released
      *
      * @param pressed true if the sprint key is pressed, false if the sprint key is
      *                released
      */
-    @Inject(method = "setPressed", at = @At("HEAD"))
+    @Inject(method = "setPressed", at = @At("TAIL"))
     private void onSetPressed(boolean pressed, CallbackInfo ci) {
         if (MinecraftClient.getInstance().options.getSprintToggled().getValue()) {
             KeyBinding key = (KeyBinding) (Object) this;
@@ -31,19 +29,13 @@ public class ToggleSprintDetectMixin {
             if (keyId.equals("key.sprint")) {
                 if (pressed) {
                     StatekeeperClient.setSprintState(true);
+                    StatekeeperClient.LOGGER.info("sprint key pressed");
                 } else {
                     StatekeeperClient.setSprintState(false);
+                    StatekeeperClient.LOGGER.info("sprint key released");
                 }
             }
         }
     }
 
-    // /**
-    // * get the state of the sprint key
-    // *
-    // * @return true if the sprint key is pressed, false otherwise
-    // */
-    // public static boolean getSprintState() {
-    // return isSprintState;
-    // }
 }
