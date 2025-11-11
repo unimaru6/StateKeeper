a66ca33e1931da1e7fd8b370b97ef92b5ed40235
uniun <uniuniunimaru6@gmail.com>
2025-11-11 22:51:02 +0900

    Updated F3B detection method

--- src/client/java/com/github/unimaru6/statekeeper/client/mixin/RenderHitboxDetectMixin.java
+++ src/client/java/com/github/unimaru6/statekeeper/client/mixin/RenderHitboxDetectMixin.java
@@ -1,5 +1,6 @@
 package com.github.unimaru6.statekeeper.client.mixin;
 
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -7,14 +8,12 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 
 import com.github.unimaru6.statekeeper.client.StatekeeperClient;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EntityRenderDispatcher.class)
-public class RenderHitboxDetectMixin {
-    @Inject(method = "shouldRenderHitboxes", at = @At("RETURN"), cancellable = true)
-    private void onShouldRenderHitboxes(CallbackInfoReturnable<Boolean> cir) {
-        boolean value = cir.getReturnValue();
-        // RETURNの後に値が反転する
-        StatekeeperClient.setRenderHitboxesState(!value);
+public abstract class RenderHitboxDetectMixin {
+    @Inject(method = "setRenderHitboxes", at = @At("TAIL"))
+    private void onSetRenderHitboxes(boolean renderHitboxes, CallbackInfo ci) {
+        StatekeeperClient.setRenderHitboxesState(renderHitboxes);
     }
 }
