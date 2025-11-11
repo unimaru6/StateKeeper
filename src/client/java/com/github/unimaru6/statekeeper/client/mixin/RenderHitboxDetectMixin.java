aa7e1247e5ce8c7e78ddbed0de956b2a3327ed2d
uniun <uniuniunimaru6@gmail.com>
2025-11-11 22:55:57 +0900

    reformat

--- src/client/java/com/github/unimaru6/statekeeper/client/mixin/RenderHitboxDetectMixin.java
+++ src/client/java/com/github/unimaru6/statekeeper/client/mixin/RenderHitboxDetectMixin.java
@@ -1,6 +1,5 @@
 package com.github.unimaru6.statekeeper.client.mixin;
 
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
